91b27da7d1c73897cf371a34369581fce2886443
Kenny Doan <kennydoan110999@gmail.com>
2021-11-23 12:04:06 -0800

    VFX

--- CMPM121Final/Assets/Scripts/WarheadExplosion.cs
+++ CMPM121Final/Assets/Scripts/WarheadExplosion.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class WarheadExplosion : MonoBehaviour
 {
     public List<GameObject> hitTargets;
+    public GameObject explosionSphere;
 
     private void Awake()
     {
-        hitTargets = new List<GameObject>();        
+        hitTargets = new List<GameObject>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        explosionSphere.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, .5f);
+        explosionSphere.transform.localScale = new Vector3(0, 0, 0);
+        LeanTween.scale(explosionSphere, new Vector3(4, 4, 4), .1f);
+        LeanTween.alpha(explosionSphere, 0, .2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
