998d7bcbae53e43483e9db65d1c5268efecd2bfe
ownagesauce <74935837+ownagesauce@users.noreply.github.com>
2021-11-28 16:58:51 -0800

    Revert "Merge branch 'main' of https://github.com/Tinnfoil/CMPM121Final"
    This reverts commit 841eb131319e5ed4a2ad25204e0eb0b6436a841b, reversing
    changes made to b665c1248ee2d61ce3fa0914d6f332a471a618a4.

--- CMPM121Final/Assets/Scripts/WarheadExplosion.cs
+++ CMPM121Final/Assets/Scripts/WarheadExplosion.cs
@@ -5,47 +5,29 @@ using UnityEngine;
 public class WarheadExplosion : MonoBehaviour
 {
     public List<GameObject> hitTargets;
-    public GameObject explosionSphere;
-    private bool effecting = true;
 
     private void Awake()
     {
-        hitTargets = new List<GameObject>();
+        hitTargets = new List<GameObject>();        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        explosionSphere.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, .5f);
-        explosionSphere.transform.localScale = new Vector3(0, 0, 0);
-        LeanTween.scale(explosionSphere, new Vector3(4, 4, 4), .1f);
-        LeanTween.alpha(explosionSphere, 0, .2f);
-        Invoke("DisableSelf", Time.deltaTime);
-    }
-
-    public void DisableSelf()
-    {
-        enabled = false;
-        effecting = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!hitTargets.Contains(other.gameObject.transform.root.gameObject) && effecting)
+        if (!hitTargets.Contains(other.gameObject))
         {
-            hitTargets.Add(other.gameObject.transform.root.gameObject);
-            if (other.GetComponentInParent<FirstPersonController>())
-            {
-                other.GetComponentInParent<FirstPersonController>().AddImpact((other.transform.position - transform.position).normalized, 30f);
-                other.GetComponentInParent<FirstPersonController>().Grounded = false;
-                other.GetComponentInParent<FirstPersonController>()._verticalVelocity = 2;
-            }
+            hitTargets.Add(other.gameObject);
             // Add force to this target
         }
     }
